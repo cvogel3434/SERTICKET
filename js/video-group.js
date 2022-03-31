62bcfa154cc16295695bc24b60313bb438d28cb0
cvogel3434 <christianv@vogelheating.com>
2022-03-31 09:39:04 -0500

    stage 1 video gallery

--- js/video-group.js
+++ js/video-group.js
@@ -3,6 +3,20 @@
 
 */
 
-var vidgroup = {
-  
-}
+var vidgroup = [
+  'Pre Comvort Consultation':{
+    src:'https://service-test2.s3.us-east-2.amazonaws.com/Pre+Comfort+Consultation.mov'
+  },
+  'Pre Service Call':{
+    src:'https://service-test2.s3.us-east-2.amazonaws.com/Pre+Service+Call.mov'
+  },
+  'Repair or Replace, No Remporse Program':{
+    src:'https://service-test2.s3.us-east-2.amazonaws.com/Repair+or+Replace%2C+No+Remporse+Program.mov'
+  },
+  'Rewards Membership':{
+    src:'https://service-test2.s3.us-east-2.amazonaws.com/Rewards+Membership.mov'
+  },
+  'Service Warranty':{
+    src:'https://service-test2.s3.us-east-2.amazonaws.com/Service+Warranty.mov'
+  }
+]
