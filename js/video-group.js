644003541af78bc61fae0bf5d2628040bc17311a
cvogel3434 <christianv@vogelheating.com>
2022-04-14 14:37:13 -0500

    videos: Fighting a loading problem. Compressed all videos and changed the format from .mov -> .mp4
    videos have been uploaded to aws, and the urls updated in the site.

--- js/video-group.js
+++ js/video-group.js
@@ -14,19 +14,19 @@ var viddom = {
 
 var vidgroup = {
   'Pre Comfort Consultation':{
-    src:'https://service-test2.s3.us-east-2.amazonaws.com/Pre+Comfort+Consultation.mov'
+    src:'https://service-test2.s3.us-east-2.amazonaws.com/Pre+Comfort+Consultation.mp4'
   },
   'Pre Service Call':{
-    src:'https://service-test2.s3.us-east-2.amazonaws.com/Pre+Service+Call.mov'
+    src:'https://service-test2.s3.us-east-2.amazonaws.com/Pre+Service+Call.mp4'
   },
   'Repair or Replace, No Remorse Program':{
-    src:'https://service-test2.s3.us-east-2.amazonaws.com/Repair+or+Replace%2C+No+Remporse+Program.mov'
+    src:'https://service-test2.s3.us-east-2.amazonaws.com/Repair+or+Replace%2C+No+Remporse+Program.mp4'
   },
   'Rewards Membership':{
-    src:'https://service-test2.s3.us-east-2.amazonaws.com/Rewards+Membership.mov'
+    src:'https://service-test2.s3.us-east-2.amazonaws.com/Rewards+Membership.mp4'
   },
   'Service Warranty':{
-    src:'https://service-test2.s3.us-east-2.amazonaws.com/Service+Warranty.mov'
+    src:'https://service-test2.s3.us-east-2.amazonaws.com/Service+Warranty.mp4'
   }
 }
 
